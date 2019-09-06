da19905e43f1edf01ba93e66623cf869983f5b2b
Popinho96 <antonio.popic@gmail.com>
2019-09-07 00:23:35 +0200

    added components and split them into smaller pieces and added styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/homepage/homepage'
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {}
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <HomePage />
+    </div>  
+    );
+  }
 }
 
 export default App;
