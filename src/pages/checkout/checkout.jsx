332cd08cf13a65fc780c168ab5f0b70f7aeae258
Popinho96 <antonio.popic@gmail.com>
2019-10-03 21:19:37 +0200

    implemented stripe payments, updated favicon and title

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart-selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item';
+import StripeCheckoutButton from './../../components/stripe-button/stripe-button';
 
 import './checkout.scss';
 
@@ -33,6 +34,12 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className="text-warning">
+            *Please use the following test credit card for payments*
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 )
 
